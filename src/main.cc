af39f85dc5b794e0c8b85464603e549d791a3a4e
gpablosl <gpablosleon15@gmail.com>
2020-08-21 08:56:00 -0700

    commit 2

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,32 @@
 #include<iostream>
 
+void PrintHello(const char* charArr)
+{
+    std::cout << charArr;
+}
+
+
+int Add(int a, int b)
+{
+return a + b;
+}
+
+void PrintArrStr(const char* arrString)
+{
+    for(int i = 0; i < 5; i++)
+    {
+        std::cout << arrString + i << std::endl;
+    }
+}
+
 int main()
 {
-std::cout <<"Hello world";
+std::cout << "add: " << Add(1,2) << std::endl;
+
+
+const char* myString{"Hello"};
+PrintArrStr(myString);
 std::cin.get();
+
 return 0;
 }
